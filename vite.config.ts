d06eef002250e294ac71ab5415c38e72199b7a3e
Andygggg <andyhello31468@gmail.com>
2025-01-03 16:54:01 +0800

    fix: 修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/study_react/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true,
+    host: '127.0.0.1',
+    port: 8078,
+    proxy: undefined,
+    cors: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 })
